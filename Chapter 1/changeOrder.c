4db676f44fab5a2a462e4444fe8c4eb6b0f32941
Helianus <xinyuhelianushu@gmail.com>
2017-09-03 19:51:00 -0500

    Update changeOrder.c

--- Chapter 1/changeOrder.c
+++ Chapter 1/changeOrder.c
@@ -3,6 +3,6 @@
 int main() {
 	int n;
 	scanf("%d", &n); // scanf must has '&'.
-	printf("%d%d%d\n", n%10, n/10%10, n/100); 
+	printf("%d%d%d\n", n%10, n/10%10, n/100); // seperate the ones, tens, hundreds.
 	return 0;
 }
