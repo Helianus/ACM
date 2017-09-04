a9ca535913d9d9a24854e0ba9d9f6e8293d2baee
Helianus <xinyuhelianushu@gmail.com>
2017-09-03 19:44:44 -0500

    Update changeOrder.c

--- Chapter 1/changeOrder.c
+++ Chapter 1/changeOrder.c
@@ -2,7 +2,7 @@
 
 int main() {
 	int n;
-	scanf("%d", &n);
-	printf("%d%d%d\n", n%10, n/10%10, n/100);
+	scanf("%d", &n); // scanf must has '&'.
+	printf("%d%d%d\n", n%10, n/10%10, n/100); 
 	return 0;
 }
