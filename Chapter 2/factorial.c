842ca8b3da18b574e49fb51b8db1667855cfe7cf
Helianus <xinyuhelianushu@gmail.com>
2017-09-21 14:18:23 -0500

    Create factorial.c

---
+++ Chapter 2/factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+
+// Input n, and compute the last 6 numbers of S = 1! + 2! + 3! + ... + n!
+// and output the time the function used.
+int main() {
+	const int MOD = 1000000;
+	int n, S = 0;
+	scanf("%d", &n);
+	if(n > 25) { 
+		n = 25; // Because after 25!, the factorial has only six 0s in the end
+		for(int i = 1; i <= n; i++) {
+			int factorial = 1;
+			for(int j = 1; j <= i; j++)
+				factorial = (factorial * j % MOD);
+			S = (S + factorial) % MOD;
+		}
+	}
+	printf("%d\n", S);
+	printf("Time used = %.2f\n", (double) clock() / CLOCKS_PER_SEC);
+	return 0;
+}
